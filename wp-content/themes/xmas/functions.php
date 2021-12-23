7328cb4f79d8bcad84522d5c24c48852b3c8db2c
Tearat <raiiliiat@yandex.ru>
2021-12-23 18:44:18 +0500

    Clear debug

--- wp-content/themes/xmas/functions.php
+++ wp-content/themes/xmas/functions.php
@@ -8,10 +8,6 @@ function xmas_add_scripts() {
   wp_enqueue_style('fancybox', '//cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css');
   wp_enqueue_style('main', get_template_directory_uri() . '/css/style.css');
 
-  // <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-  // <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
-  // <script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
-  // <script src="js/main.js"></script>
   wp_enqueue_script('swiper', '//unpkg.com/swiper/swiper-bundle.min.js', array(), '1.0', true);
 
   // jquery
